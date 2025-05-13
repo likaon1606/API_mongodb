69103f9073f05d10deae0194823100391bff56b2
likaon1606 <ariel.fuentesgarcia@gmail.com>
2025-05-13 15:07:18 -0600

    schema definited

--- models/mascotas.js
+++ models/mascotas.js
@@ -1,30 +1,27 @@
-import { ObjectId } from "mongodb";
-import dbClient from "../config/dbClient.js";
+import Mascota from '../schemas/mascotas.js'
 
 class mascotasModel {
   async create(mascota) {
-    const colMascotas = dbClient.db.collection('mascotas')
-    return await colMascotas.insertOne(mascota)
+    return await Mascota.create(mascota)
   }
 
   async update(id, mascota) {
-    const colMascotas = dbClient.db.collection('mascotas')
-    return await colMascotas.updateOne({ _id: new ObjectId(id) }, { $set: mascota })
+    return await Mascota.findOneAndUpdate(id, mascota, {
+      new: true,
+      // runValidators: true
+    })
   }
 
   async delete(id) {
-    const colMascotas = dbClient.db.collection('mascotas')
-    return await colMascotas.deleteOne({ _id: new ObjectId(id) })
+    return await Mascota.findOneAndDelete(id)
   }
 
   async getAll() {
-    const colMascotas = dbClient.db.collection('mascotas')
-    return await colMascotas.find().toArray()
+    return await Mascota.find()
   }
 
   async getOne(id) {
-    const colMascotas = dbClient.db.collection('mascotas')
-    return await colMascotas.findOne({ _id: new ObjectId(id) })
+    return await Mascota.findById(id)
   }
 }
 
